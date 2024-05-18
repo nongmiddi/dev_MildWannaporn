4ad52a43d8409d52a2a4f2682557db2e1e46593f
budsaraporn <budsaraporn.a.ep@gmail.com>
2024-05-18 09:36:20 +0700

    init app

---
+++ lib/home_fern.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeFern extends StatefulWidget {
+  const HomeFern({super.key});
+
+  @override
+  State<HomeFern> createState() => _HomeFernState();
+}
+
+class _HomeFernState extends State<HomeFern> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
